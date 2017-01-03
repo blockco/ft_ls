ec904fa8b21b7810b3333f6e762b7665d2fd3540
robert passafaro <robert.passafaro@gmail.com>
2017-01-03 12:21:17 -0800

    Add files via upload

---
+++ ft_ls.h
@@ -0,0 +1,13 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include "./libft/libft.h"
+
+typedef	struct	s_flags
+{
+	
+}				t_flags;
